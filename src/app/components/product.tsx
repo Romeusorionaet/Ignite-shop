a1fb9dae00c22636ac7a833bd00a438858c5e59c
romeusorionaet <romeusoares14569@gmail.com>
2023-07-14 15:17:22 -0300

    chore stripe

--- src/app/components/product.tsx
+++ src/app/components/product.tsx
@@ -1,26 +1,54 @@
-import Image from 'next/image'
+'use client'
 
-interface shirtProps {
-    shirt: string
+import Image from 'next/image'
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
+interface ProductsProps {
+    products: {
+        id: string
+        name: string;
+        imageUrl: string;
+        price: number | null
+    }[]
 }
 
-export function Product({shirt}: shirtProps) {
-    return(
-        <a href='#' className='group'>
-            <div 
-            className='flex justify-center bg-gradient-to-b from-[#1ea483] to-[#7456d4] rounded-lg p[0.25rem] cursor-pointer relative'>
-                <Image 
-                className='object-cover' 
-                width={520} height={480} 
-                src={shirt} 
-                alt='' 
-                />
+export function Product({products}: ProductsProps) {
+    const [sliderRef] = useKeenSlider({
+        slides: {
+        perView: 2.5,
+        spacing: 48,
+        }
+    })
 
-                <div 
-                className='text-lg font-bold absolute bottom-[0.25rem] left-[0.25rem] right-[0.40rem] p-[3.2rem] rounded-md bg-black/60 h-[6rem] animation-hover-hidden group-hover:animation-hover-show '>
-                <span>Camiseta Beyond the Limits</span> <strong className='text-xl text-green300'>R$ 79,90</strong>
-                </div>
-            </div>
-        </a>
+    return(
+        <div ref={sliderRef} className='keen-slider flex'>
+                {
+                    products.map((product) => {
+                        return(
+                            <a 
+                            key={product.id}
+                            href='#' 
+                            className='group keen-slider__slide'
+                            >
+                                <div 
+                                className='flex justify-center bg-gradient-to-b from-[#1ea483] to-[#7456d4] rounded-lg p[0.25rem] cursor-pointer relative'>
+                                    <Image 
+                                    className='object-cover' 
+                                    width={520} height={480} 
+                                    src={product.imageUrl} 
+                                    alt='' 
+                                    />
+
+                                    <div 
+                                    className='text-lg font-bold absolute bottom-[0.25rem] left-[0.25rem] right-[0.40rem] p-[3.2rem] rounded-md bg-black/60 h-[6rem] animation-hover-hidden group-hover:animation-hover-show '>
+                                    <span>{product.name}</span> <strong className='text-xl text-green300'>R$ {product.price}</strong>
+                                    </div>
+                                </div>
+                            </a>
+                        )
+                    })
+                }
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,44 @@
-'use client'
-
 import { Product } from "./components/product";
-import { useKeenSlider } from 'keen-slider/react'
-import 'keen-slider/keen-slider.min.css'
+import { stripe } from "./lib/stripe";
+import Stripe from "stripe";
 
-export default function Home() {
-  const [sliderRef] = useKeenSlider({
-    slides: {
-      perView: 2.5,
-      spacing: 48,
+export interface ProductsProps {
+  id: string
+  name: string
+  imageUrl: string
+  price: number | null
+}
+
+export default async function Home() {
+  const data = await getData()
+  const products: ProductsProps[] = data.props.products
+
+  return (
+    <main>
+      <Product products={products} />
+    </main>
+  )
+}
+
+const getData = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount && price.unit_amount / 100
     }
   })
 
-  return (
-    <main
-    ref={sliderRef}
-    className='keen-slider flex'>
-      <Product shirt={image} />
-
-    </main>
-  )
+  return {
+    props: {
+      products
+    }
+  }
 }

3303d64bca2046ffff4e3d252fbdd2b5720df48a
romeusorionaet <romeusoares14569@gmail.com>
2023-07-21 14:27:49 -0300

    change url

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -20,7 +20,7 @@ export default async function handler(
     return response.status(405).json({ error: 'Method not allowed' })
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
+  const successUrl = `${process.env.NEXT_URL}/success`
   const cancelUrl = `${process.env.NEXT_URL}/`
 
   const transformItems = cartItem.map((item: ProductsProps) => ({
